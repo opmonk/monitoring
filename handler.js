eedc56f030cb43fa47ce74f128e44b1fb00cd1d0
Ben Nguyen <bv@Bens-MacBook-Pro.local>
2019-09-25 11:22:39 -0700

    refactored handler.js

--- handler.js
+++ handler.js
@@ -16,17 +16,73 @@ const db = mysql.createConnection({
 const channel = process.env.SLACK_CHANNEL || 'ben-test';
 
 
-const formatCrawlerAlert = (results) => {
-  let text = "";
-  results.forEach(result => {
-    text+=sprintf('%-10s%10f\n',result["status"],result["Total Count"]);
-  });
-  return "```" + text + "```";
-}
+/**
+* Calculates useful crawler summaries, such as percent finished and unsuccessful
+* @params {object} DB results object that contains crawling status with their counts
+* @returns {object} summary object
+*/
+const calculateSummary = (results) => {
+  let total = 0;
+  let unsuccessful = 0; // failed + cancelled
+  let outstanding = 0; // crawling + scheduled
 
+  results.forEach(result => {
+    const count = result["Total Count"];
+    const status = result["status"];
+
+    if (status == 'cancelled' || status == 'failure') unsuccessful += count;
+    else if (status == 'scheduled' || status == 'crawling') outstanding += count;
+    total += count;
+  });
+
+  const finishedPercentage = ((total - outstanding) / total * 100).toFixed(2);
+  const unsuccessfulPercentage = (unsuccessful / total * 100).toFixed(2);
+  return {
+    finishedPercentage,
+    unsuccessfulPercentage
+  };
+};
+
+/**
+* Generates & formats Slack crawler alert message
+* @params {object} DB results data along with summary data (and start/end info)
+* @returns {string} slack alert text
+*/
+const formatCrawlerAlert = (results) => {
+  const {
+    data,
+    summary,
+    summary: {
+      startTime,
+      endTime,
+      unsuccessfulPercentage,
+      finishedPercentage
+    }
+  } = results;
+
+  const [today, startHour] = startTime.split(" ");
+  const endHour = endTime.split(" ")[1];
+
+  let title = `:alarm_clock:  *Crawler Alert*: ${today} (${startHour} to ${endHour})\n`;
+  let text = "";
+
+  data.forEach(item => {
+    text += sprintf('%-10s%10f\n', item["status"], item["Total Count"]);
+  });
+
+  title += `:white_check_mark:  *Finished*: ${summary.finishedPercentage}%\n`;
+  title += `:x:  *Unsuccessful*: ${summary.unsuccessfulPercentage}%\n`;
+  return title + "```" + text + "```";
+};
+
+/**
+* Generates Slack alert
+* @params {void}
+* @returns {string} returns string body of slack alert text. Also posts to slack.
+*/
 module.exports.generateSlackAlert = async event => {
   const today = moment().date();
-  const isOddDay = today%2==1;
+  const isOddDay = today % 2 == 1;
   let startTime;
   let endTime;
   if (isOddDay && today != 31) {
@@ -42,9 +98,15 @@ module.exports.generateSlackAlert = async event => {
   `;
 
   const promise = new Promise(function(resolve, reject) {
-    db.query(sql,[startTime,endTime], async function (error, results, fields) {
-      let text = "*Crawler Alert:  " + moment().format("YYYY-MM-DD") + "*\n" + formatCrawlerAlert(results);
-      console.log(text);
+    db.query(sql, [startTime, endTime], async function(error, results, fields) {
+      const summary = calculateSummary(results);
+      summary.startTime = startTime;
+      summary.endTime = endTime;
+      let text = formatCrawlerAlert({
+        data: results,
+        summary
+      });
+      console.log("Slack Text:",text);
 
       request({method: 'POST', url: 'https://hooks.slack.com/services/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', json: true, body: {'username': 'IPSHARK-BOT','text': text, 'channel': channel}}, function(err, res, body) {
         if (err) console.log(err);
@@ -57,10 +119,6 @@ module.exports.generateSlackAlert = async event => {
       reject(Error(e));
     });
   })
-  
+
   return promise;
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
-
